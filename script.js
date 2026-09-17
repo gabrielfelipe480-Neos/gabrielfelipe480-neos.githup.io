b26041cd7048c88fde5390736749f8b66a8229f5
gabrielfelipe480-Neos <gabrielfelipeoliveira480@gmail.com>
2026-09-16 21:07:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 
+
 //================================
-//BOTÃO "CONHEÇA MEU TRABALHO"
+// BOTÃO "CONHEÇA MEU TRABALHO"
 //================================
 
 const botaoSobre = document.querySelector("#botaoSobre");
+const titulo = document.querySelector("#hero h2");
+const hero = document.querySelector("#hero");
 
+// Guarda o estado do botão
+let ativo = false;
+
+// Quando o botão for clicado
 botaoSobre.addEventListener("click", function () {
 
-    const titulo = document.querySelector("#hero h2");
+    if (ativo === false) {
 
-    titulo.textContent = "Bem-vindo ao meu portfólio! 🚀";
-
-    botaoSobre.textContent = "Obrigado pela visita!";
+        // Ativa a interação
+        titulo.textContent = "Bem-vindo ao meu portfólio! | Conheça meu trabalho";
+        botaoSobre.textContent = "Obrigado pela visita! | Desenvolvedor Front-end";
+        hero.classList.add("hero-ativo");
+        ativo = true;
+    }       
 
 });
-
-
-
-
-
